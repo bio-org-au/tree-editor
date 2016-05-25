bfc985403a8a136b5dc351f63576e43c77136e88
Paul Murray <pmurray@anbg.gov.au>
2016-05-25 13:09:00 +1000

    NSL-4 - tree - Taxonomic tree functionality changes to make this deployable in our hosting configuration

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,6 +11,9 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
+grails.config.locations = ["file:${userHome}/.nsl/${appName}-config.groovy"]
+
+
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
@@ -88,12 +91,6 @@ grails.hibernate.osiv.readonly = false
 environments {
     development {
         grails.logging.jul.usebridge = true
-
-        treeEditor {
-            servicesUrl = 'http://localhost:8080/services'
-            pagesUrl = 'http://localhost:8081/tree-editor'
-        }
-
     }
     production {
         grails.logging.jul.usebridge = false
---
+++ scripts/SetWarPath.groovy
@@ -0,0 +1,48 @@
+/*
+    Copyright 2015 Australian National Botanic Gardens
+
+    This file is part of NSL services project.
+
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not
+    use this file except in compliance with the License. You may obtain a copy
+    of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+// this file was copied from the services app
+
+includeTargets << grailsScript("_GrailsInit")
+includeTargets << grailsScript("_GrailsArgParsing")
+
+target(setWarPath: "set the context path of the war file for Tomcat deploys.") {
+
+    depends(parseArguments)
+
+    if (argsMap["params"].size() != 1) {
+        event("StatusError", [statusError])
+        return
+    }
+
+    File targetDir = new File('target')
+    String warPath = argsMap.params[0].toString()
+
+    if(targetDir.exists() && targetDir.isDirectory()) {
+        File[] warFiles = targetDir.listFiles(new FilenameFilter() {
+            @Override
+            boolean accept(File dir, String name) {
+                return name.endsWith('.war')
+            }
+        })
+        warFiles.each { File warFile ->
+            String path = warFile.canonicalPath - warFile.name
+            warFile.renameTo("${path}/${warPath}#${warFile.name}")
+        }
+    }
+}
+
+setDefaultTarget(setWarPath)
