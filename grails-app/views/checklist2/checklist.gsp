a2d0fb67679afa28e8d59e9a05b05544fe3d21af
Paul Murray <pmurray@anbg.gov.au>
2017-02-03 12:30:37 +1100

    Save work before doing stuff without mapper links

--- grails-app/views/checklist2/checklist.gsp
+++ grails-app/views/checklist2/checklist.gsp
@@ -2,11 +2,11 @@
 <html>
 <head>
     <meta name="layout" content="pages"/>
-    <g:javascript src="ng/checklist/checklist.js"/>
+    <g:javascript src="ng/checklist2/checklist.js"/>
     <asset:stylesheet src="checklist.css"/>
 </head>
 
 <body>
-<div data-list-type="Checklist" data-root-uri="${rootUri}" data-focus-uri="${focusUri}" checklist></div>
+<div data-arrangement-uri="${arrangementUri}" data-focus-uri="${focusUri}" checklist></div>
 </body>
 </html>
---
+++ web-app/js/ng/checklist2/checklist.js
@@ -0,0 +1,180 @@
+
+function putErrorOnPage($rootScope, response) {
+    if (response.data && response.data.msg) {
+        $rootScope.msg = response.data.msg;
+    }
+    else if (response.data && response.data.status) {
+        $rootScope.msg = [
+            {
+                msg: 'URL',
+                body: response.config.url,
+                status: 'info'
+            },
+            {
+                msg: response.data.status,
+                body: response.data.reason,
+                status: 'danger'
+            }
+        ];
+    }
+    else {
+        $rootScope.msg = [
+            {
+                msg: 'URL',
+                body: response.config.url,
+                status: 'info'
+            },
+            {
+                msg: response.status,
+                body: response.statusText,
+                status: 'danger'
+            }
+        ];
+    }
+}
+
+var ChecklistController = ['$scope', '$rootScope', '$http', 'jsonCache', function ($scope, $rootScope, $http, jsonCache) {
+    $scope.checklist_scope = $scope;
+
+    $scope.arrangement = jsonCache.needJson($scope.arrangementUri);
+    $scope.path = null;
+    $scope.pathState = {loading: false, loaded: false};
+    $scope.cursorUri = $scope.focusUri;
+
+    $scope.appendBranchToPath = function() {};
+
+    $scope.nodeUI = {}; // this is where I remember which nodes are open, etc
+    $scope.getNodeUI = function (uri) {
+        if (!$scope.nodeUI[uri]) {
+            $scope.nodeUI[uri] = {open: false};
+        }
+        return $scope.nodeUI[uri];
+    };
+
+    $scope.clickPath = function(index) {
+        console.log("path item " + index + " clicked");
+    };
+
+
+    $http({
+        method: 'GET',
+        url: $rootScope.servicesUrl + "/TreeJsonView/nodePath",
+        params: {arrangement: $scope.arrangementUri, focus: $scope.focusUri}
+    })
+        .then(function (response) {
+            console.log("GET PATH SUCCESS");
+            $scope.pathState.loading = false;
+            $scope.pathState.loaded = true;
+            $scope.path = response.data.result;
+
+            $scope.focusJson = $scope.path[$scope.path.length - 1];
+
+            if (!$scope.cursorUri) {
+                $scope.cursorUri = $scope.focusJson.uri
+            }
+
+            for (i in $scope.path) {
+                $scope.getNodeUI($scope.path[i].uri).open = true;
+            }
+
+        }, function (response) {
+            console.log("GET PATH FAIL");
+            console.log(response);
+            $scope.pathState.loading = false;
+            putErrorOnPage($rootScope, response);
+        });
+}];
+
+app.controller('Checklist', ChecklistController);
+
+var checklistDirective = [function () {
+    return {
+        templateUrl: pagesUrl + "/ng/checklist2/checklist.html",
+        controller: ChecklistController,
+        scope: {
+            arrangementUri: "@",
+            focusUri: "@"
+        }
+    };
+}];
+
+app.directive('checklist', checklistDirective);
+
+
+var BranchController = ['$scope', '$rootScope', '$http', 'jsonCache', function ($scope, $rootScope, $http, jsonCache) {
+    $scope.checklist_scope = $scope.$parent.checklist_scope;
+
+    $scope.branchState = {loading: false, loaded: false};
+
+    $scope.UI = $scope.checklist_scope.getNodeUI($scope.json.uri)
+    
+    $scope.selectMe = function() {
+        $scope.checklist_scope.cursorUri = $scope.json.uri;
+        $scope.checklist_scope.path = [];
+        $scope.appendBranchToPath();
+    };
+
+    $scope.appendBranchToPath = function() {
+        $scope.$parent.appendBranchToPath();
+        $scope.checklist_scope.path[$scope.checklist_scope.path.length] = $scope.json
+    };
+
+    var deregisterLoading = $scope.$watch("UI.open", function () {
+            if ($scope.UI.open && $scope.arrangementUri && !$scope.branchState.loading) {
+
+                console.log("EXECUTING BRANCH FETCH");
+                console.log("$scope.arrangementUri" + $scope.arrangementUri);
+                console.log("$scope.json.uri" + $scope.json.uri);
+
+                // only do this once
+                deregisterLoading();
+
+                $scope.branchState.loading = true;
+
+                $http({
+                    method: 'GET',
+                    url: $rootScope.servicesUrl + "/TreeJsonView/nodeBranch",
+                    params: {arrangement: $scope.arrangementUri, focus: $scope.json.uri}
+                })
+                    .then(
+                        function (response) {
+                            console.log("GET BRANCH SUCCESS");
+                            $scope.branchState.loading = false;
+                            $scope.branchState.loaded = true;
+                            $scope.branch = response.data.result
+                        },
+                        function (response) {
+                            console.log("GET BRANCH FAIL");
+                            console.log(response);
+                            $scope.branchState.loading = false;
+                            putErrorOnPage($rootScope, response);
+                        });
+            }
+        }
+    );
+
+}];
+
+app.controller('Branch', BranchController);
+
+var branchDirective = ['RecursionHelper', function (RecursionHelper) {
+    return {
+        templateUrl: pagesUrl + "/ng/checklist2/branch.html",
+        controller: BranchController,
+        scope: {
+            arrangementUri: '@',
+            json: '=',
+        },
+        compile: function (element) {
+            return RecursionHelper.compile(element, function (scope, iElement, iAttrs, controller, transcludeFn) {
+                // Define your normal link function here.
+                // Alternative: instead of passing a function,
+                // you can also pass an object with
+                // a 'pre'- and 'post'-link function.
+            });
+        }
+    };
+}];
+
+app.directive('branch', branchDirective);
+
